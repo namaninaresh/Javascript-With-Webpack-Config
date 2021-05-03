fb99208a6dce8f9cb02f270ced4091167a0746c5
namaninaresh <namaninaresh505@gmail.com>
2021-05-03 15:44:19 +0530

    First Commit with all Configured

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import '../assets/css/style.css';
+import "../assets/css/style.css";
 
-const app = document.getElementById('app');
-app.innerHTML = '<h1>JavaScript Basics</h1>';
+const app = document.getElementById("app");
+app.innerHTML = "<h1>JavaScript Basics</h1>";
+
+var numbers = [1, 2, 3, 5, ["name", "world"]];
+
+let clone = numbers.map((ele) => ele);
+
+//let clone = JSON.parse(JSON.stringify(numbers));
+
+clone[4][0] = "hellow";
+
+console.log(numbers.splice(1));
+console.log(clone);
